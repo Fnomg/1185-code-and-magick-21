1bfb0794980ea18fb17ef378440ac980f384ff4d
Anna <annie_z@bk.ru>
2020-10-12 14:55:57 +0300

    make submit data

--- js/setup.js
+++ js/setup.js
@@ -2,8 +2,9 @@
 
 const ARR_NAME = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const ARR_FAMILY = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const ARR_COAT_COLAR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const ARR_COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const ARR_EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
+const ARR_FIREBALL_COLOR = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
 const MIN = 0;
 const COUNT_WIZARDS = 4;
 const setup = document.querySelector(`.setup`);
@@ -11,8 +12,10 @@ const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
 const similarListElement = setup.querySelector(`.setup-similar-list`);
 const setupOpen = document.querySelector(`.setup-open`);
 const setupClose = document.querySelector(`.setup-close`);
-const setupOpenIcon = setupOpen.querySelector(`.setup-open-icon`);
-const setupUserName = setup.querySelector(`.setup-user-name`)
+const setupUserName = setup.querySelector(`.setup-user-name`);
+const wizardCoat = document.querySelector(`.wizard-coat`);
+const wizardEye = document.querySelector(`.wizard-eyes`);
+const wizardFireball = document.querySelector(`.setup-fireball-wrap`);
 
 document.querySelector(`.setup-similar`).classList.remove(`hidden`);
 
@@ -25,7 +28,7 @@ const getRandomNumber = function (min, max) {
 const makeWizard = function () {
   const wizard = {
     name: `${ARR_NAME[getRandomNumber(MIN, ARR_NAME.length - 1)]} ${ARR_FAMILY[getRandomNumber(MIN, ARR_FAMILY.length - 1)]}`,
-    coatColor: ARR_COAT_COLAR[getRandomNumber(MIN, ARR_COAT_COLAR.length - 1)],
+    coatColor: ARR_COAT_COLOR[getRandomNumber(MIN, ARR_COAT_COLOR.length - 1)],
     eyesColor: ARR_EYES_COLOR[getRandomNumber(MIN, ARR_EYES_COLOR.length - 1)]
   };
   return wizard;
@@ -58,7 +61,7 @@ similarListElement.appendChild(fragment);
 
 const onPopupEscPress = function (evt) {
   if (document.activeElement === setupUserName) {
-    return evt
+    evt.preventDefault();
   } else if (evt.key === `Escape`) {
     evt.preventDefault();
     closePopup();
@@ -72,7 +75,7 @@ const openPopup = function () {
 
 const closePopup = function () {
   setup.classList.add(`hidden`);
-  document.removeEventListener(`keydown`, onPopupEscPress)
+  document.removeEventListener(`keydown`, onPopupEscPress);
 };
 
 setupOpen.addEventListener(`click`, function () {
@@ -84,13 +87,58 @@ setupClose.addEventListener(`click`, function () {
 });
 
 setupOpen.addEventListener(`keydown`, function (evt) {
-  if (evt.key === 'Enter') {
+  if (evt.key === `Enter`) {
     openPopup();
   }
 });
 
 setupClose.addEventListener(`keydown`, function (evt) {
-  if (evt.key === `Escape` || evt.key === `Enter` ) {
+  if (evt.key === `Escape` || evt.key === `Enter`) {
     closePopup();
   }
+});
+
+const getRandomCoatColor = function () {
+  const coatColor = ARR_COAT_COLOR[getRandomNumber(MIN, ARR_COAT_COLOR.length - 1)];
+  return coatColor;
+};
+
+const setCoatColor = function () {
+  const newCoatColor = getRandomCoatColor();
+  wizardCoat.style.fill = newCoatColor;
+  document.querySelector(`[name="coat-color"]`).value = newCoatColor;
+};
+
+wizardCoat.addEventListener(`click`, function () {
+  setCoatColor();
+});
+
+const getRandomEyeColor = function () {
+  const eyeColor = ARR_EYES_COLOR[getRandomNumber(MIN, ARR_EYES_COLOR.length - 1)];
+  return eyeColor;
+};
+
+const setEyeColor = function () {
+  const newEyeColor = getRandomEyeColor();
+  wizardEye.style.fill = newEyeColor;
+  document.querySelector(`[name="eyes-color"]`).value = newEyeColor;
+};
+
+wizardEye.addEventListener(`click`, function () {
+  setEyeColor();
+});
+
+const getRandomFireballColor = function () {
+  const fireballColor = ARR_FIREBALL_COLOR[getRandomNumber(MIN, ARR_FIREBALL_COLOR.length - 1)];
+  return fireballColor;
+};
+
+const setFireballColor = function () {
+  const fireballColor = getRandomFireballColor();
+  wizardFireball.style.background = fireballColor;
+  document.querySelector(`[name="fireball-color"]`).value = fireballColor;
+};
+
+wizardFireball.addEventListener(`click`, function () {
+  setFireballColor();
 });
